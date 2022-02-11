8b8b474c2ad814c792463fc6aa6e814a4f8a5652
Lloyd Lobo <lloydlobo4@gmail.com>
2022-02-11 13:05:38 +0530

    style: :construction: backlogs feature to fetch input location weather
    Added future feature to be implemented `document.body.style.backgroundImage = "url(`https://source.unsplash.com/data/2.5/weather?q=" + name + "')";`. Comented out right now for reminder.

--- script.js
+++ script.js
@@ -1,49 +1,54 @@
 let weather = {
-  apiKey: "e0e09750f52faa72ead259c3bdf495f6",
-  fetchWeather: function (city) {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=metric&appid=" +
-        this.apiKey
-    )
-      .then((response) => response.json())
-      .then((data) => this.displayWeather(data));
-  },
-  displayWeather: function (data) {
-    const { name } = data;
-    const { icon, description } = data.weather[0];
-    const { temp, humidity } = data.main;
-    const { speed } = data.wind;
-    // console.log(name, icon, description, temp, humidity, speed);
-    document.querySelector(".city").innerText = "Weather in " + name;
-    document.querySelector(".icon").src =
-      "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".description").innerText = description;
-    document.querySelector(".temp").innerText = temp + "°";
-    document.querySelector(".humidity").innerText =
-      "Humidity " + humidity + "%";
-    document.querySelector(".wind").innerText =
-      "Wind speed: " + speed + " km/h";
-    document.querySelector(".weather").classList.remove("loading");
-    // document.body.style.backgroundImage =
-    //   "url(`https://source.unsplash.com/random/?" + name + ")";
-  },
-  search: function () {
-    this.fetchWeather(document.querySelector(".search-bar").value);
-  },
+    apiKey: "e0e09750f52faa72ead259c3bdf495f6",
+    fetchWeather: function(city) {
+        fetch(
+                "https://api.openweathermap.org/data/2.5/weather?q=" +
+                city +
+                "&units=metric&appid=" +
+                this.apiKey
+            )
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        // console.log(name, icon, description, temp, humidity, speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src =
+            "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°";
+        document.querySelector(".humidity").innerText =
+            "Humidity " + humidity + "%";
+        document.querySelector(".wind").innerText =
+            "Wind speed: " + speed + " km/h";
+        document.querySelector(".weather").classList.remove("loading");
+        // document.body.style.backgroundImage =
+        //   "url(`https://source.unsplash.com/data/2.5/weather?q=" + name + "')";
+    },
+    search: function() {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    },
 };
 
-document.querySelector(".search button").addEventListener("click", function () {
-  weather.search();
+document.querySelector(".search button").addEventListener("click", function() {
+    weather.search();
 });
 
 document
-  .querySelector(".search-bar")
-  .addEventListener("keyup", function (event) {
-    if (event.key == "Enter") {
-      weather.search();
-    }
-  });
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function(event) {
+        if (event.key == "Enter") {
+            weather.search();
+        }
+    });
 
 weather.fetchWeather("Bali");
+
+// import Blobity from "blobity";
+
+// const options = { color: "rgb(180, 180, 180)" };
+// new Blobity(options);
