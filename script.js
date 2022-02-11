3217cc4e45ab929404e6935bd54588f0406a5503
Lloyd Lobo <lloydlobo4@gmail.com>
2022-02-11 20:40:40 +0530

    refactor: :recycle: Add template literals alternative instead of string concatenation

--- script.js
+++ script.js
@@ -2,10 +2,7 @@ const weather = {
     apiKey: "e0e09750f52faa72ead259c3bdf495f6",
     fetchWeather: function(city) {
         fetch(
-                "https://api.openweathermap.org/data/2.5/weather?q=" +
-                city +
-                "&units=metric&appid=" +
-                this.apiKey
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.apiKey}`
             )
             .then((response) => response.json())
             .then((data) => this.displayWeather(data));
@@ -16,16 +13,14 @@ const weather = {
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
         // console.log(name, icon, description, temp, humidity, speed);
-        document.querySelector(".city").innerText = "Weather in " + name;
-        document.querySelector(".icon").src =
-            "https://openweathermap.org/img/wn/" + icon + ".png";
-        document.querySelector(".description").innerText = description;
-        document.querySelector(".temp").innerText = temp + "°";
-        // document.querySelector(".humidity").innerText =
-        //     "Humidity " + humidity + "%";
-        document.querySelector(".humidity").innerText = `Humidity  ${humidity}%`;
-        document.querySelector(".wind").innerText =
-            "Wind speed: " + speed + " km/h";
+        document.querySelector(".city").innerText = `Weather in  ${name}`;
+        document.querySelector(
+            ".icon"
+        ).src = `https://openweathermap.org/img/wn/${icon}.png`;
+        document.querySelector(".description").innerText = `${description}`;
+        document.querySelector(".temp").innerText = `${temp}°`;
+        document.querySelector(".humidity").innerText = `Humidity:  ${humidity}%`;
+        document.querySelector(".wind").innerText = `Wind speed:  ${speed} km/h`;
         document.querySelector(".weather").classList.remove("loading");
         // document.body.style.backgroundImage =
         //   "url(`https://source.unsplash.com/data/2.5/weather?q=" + name + "')";
